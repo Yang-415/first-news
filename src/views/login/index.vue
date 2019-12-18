c8611bee9dfc85754f37bce8c9dd302654281c41
unknown <yanzi@qq.com>
2019-12-18 22:50:08 +0800

    黑马头条PC-登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="login-bag">
     <el-card class="login-card">
-      <img src="../../assets/img/logo_index.png" alt="" />
-      <el-form :model="loginForm" :rules="loginRules">
+      <img src="../../assets/img/logo_index.png" alt />
+      <el-form :model="loginForm" :rules="loginRules" ref="myform">
         <el-form-item prop="mobile">
           <el-input placeholder="请输入手机号" style="margin-top:20px" v-model="loginForm.mobile"></el-input>
         </el-form-item>
@@ -10,15 +10,15 @@
           <el-input placeholder="请输入验证码" style="width:70%" v-model="loginForm.code"></el-input>
           <el-button style="float:right">发送验证码</el-button>
         </el-form-item>
-        <el-form-item >
+        <el-form-item prop="checkboxed">
           <el-checkbox style="float:left" v-model="loginForm.checkboxed">
             我已阅读并同意
             <span class="login-word">协议条款</span>和
             <span class="login-word">隐私协议</span>
           </el-checkbox>
-        </el-form-item >
-        <el-form-item prop="checkboxed">
-            <el-button type="primary" style="width:100%,m">登陆</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%" @click="loginSubmit">登陆</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -33,16 +33,37 @@ export default {
         mobile: '',
         code: '',
         checkboxed: false
-
       },
       loginRules: {
-        mobile: [{ required: true, message: '请输入您的手机号' }],
-        code: [{ required: true, message: '请输入您的验证码' }],
-        checkboxed: [{ validator () {} }]
+        mobile: [
+          { required: true, message: '请输入您的手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '手机号码格式不正确' }
+        ],
+        code: [
+          { required: true, message: '请输入您的验证码' },
+          { pattern: /^\d{6}$/, message: '验证码为6位数字' }
+        ],
+        checkboxed: {
+          validator (rules, value, callback) {
+            if (value) {
+              callback()
+            } else {
+              callback(new Error('您必须阅读并同意协议'))
+            }
+          }
+        }
       }
     }
+  },
+  methods: {
+    loginSubmit () {
+      this.$refs.myform.validate(function (isOk) {
+        if (isOk) {
+          console.log('校验成功')
+        }
+      })
+    }
   }
-
 }
 </script>
 
@@ -64,7 +85,7 @@ export default {
     }
     .login-word {
       color: rgb(95, 165, 223);
-    };
+    }
   }
 }
 </style>
