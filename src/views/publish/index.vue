fdbb5e7fde427944d63e790fa093bae6bd9924bd
unknown <yanzi@qq.com>
2019-12-27 19:50:49 +0800

    黑马头条PC-发表文章-发布文章-简单发布文章

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -3,7 +3,7 @@
     <breadcumb slot="header">
       <span slot="title">发布文章</span>
     </breadcumb>
-    <el-form label-width="100px" :model="formdata" :rules=rules>
+    <el-form label-width="100px" :model="formdata" :rules=rules ref="publicForm">
       <el-form-item label="标题" style="width: 60%" prop="title">
         <el-input v-model="formdata.title"></el-input>
       </el-form-item>
@@ -24,8 +24,8 @@
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">发布</el-button>
-        <el-button type="danger" plain>草稿</el-button>
+        <el-button type="primary" @click="publicArticle(false)">发布</el-button>
+        <el-button type="danger" plain @click="publicArticle(true)">草稿</el-button>
       </el-form-item>
     </el-form>
   </el-card>
@@ -40,9 +40,10 @@ export default {
         title: '',
         content: '',
         cover: {
-          type: 0
+          type: 0,
+          images: []
         },
-        channel_id: ''
+        channel_id: null
       },
       rules: {
         title: [{ required: true, message: '请输入文章标题' }, { min: 5, max: 30, message: '文章标题字数5-30之内' }],
@@ -52,13 +53,36 @@ export default {
     }
   },
   methods: {
+    //   发布校验
+    publicArticle (draft) {
+      this.$refs.publicForm.validate((isOk) => {
+        if (isOk) {
+          this.pubOrDraft(draft)
+        }
+      })
+    },
     // 获取频道数据
     getChannelsData () {
       this.$axios({ url: 'channels' }).then(result => {
         this.list = result.data.channels
       })
+    },
+    // 发布或存为草稿
+    pubOrDraft (draft) {
+      this.$axios({
+        url: '/articles',
+        method: 'post',
+        params: {
+          draft
+        },
+        data: this.formdata
+      }).then(() => {
+        // 跳转到内容列表页面
+        this.$router.push('/home/articles')
+      })
     }
   },
+
   created () {
     this.getChannelsData()
   }
